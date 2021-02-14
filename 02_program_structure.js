e6fa57238519e55116c5c6d3c074de818150d6c8
Robert Benavidez <murabitbenavidez@gmail.com>
2021-02-14 01:35:10 -0500

    Ch 2 ex 1

---
+++ 02_program_structure.js
@@ -0,0 +1,9 @@
+// Exercise: Looping a triangle
+function triangle (height){
+    for (let line = "$"; line.length < height; line += "$" ) {
+        console.log(line)
+    }
+}
+console.log(triangle(10))
+
+// Exercise: FizzBuzz
